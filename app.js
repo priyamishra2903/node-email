2e509c13c92120c89e0add8d910d2945939e32a2
Priya Mishra <mishrapriya9999@gmail.com>
2023-09-30 14:14:03 +0530

    chnges

--- app.js
+++ app.js
@@ -1,61 +1,58 @@
-// const express = require('express');
-// const path = require('path');
-// const bodyParser = require('body-parser');
-// const app = express();
-// const nodemailer = require('nodemailer');
-// const cors = require('cors');
-// const http = require('http');
-// const httpProxy = require('http-proxy');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const https = require('https');
+const fs = require('fs');
 
-// const corsOptions = {
-//   origin: '*', // Allow requests from any origin (not recommended for production)
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//   credentials: true, // Allow cookies and credentials to be sent
-// };
-
-// app.use(cors(corsOptions));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// app.get('/getData', (req, res) => {
-//   res.send("hiiii");
-// });
-
-// // HTTP-to-HTTPS proxy server setup
-// const HTTP_PORT = 80; // Port for HTTP proxy
-// const HTTPS_TARGET = {
-//   target: '127.0.0.1', // Replace with your HTTPS server's address
-//   changeOrigin: true, // Changes the host header to match the target host
-// };
-
-// const httpServer = http.createServer((req, res) => {
-//   const proxy = httpProxy.createProxyServer(HTTPS_TARGET);
-//   proxy.web(req, res, HTTPS_TARGET, (err) => {
-//     console.error('Proxy error:', err);
-//     res.status(500).send('Proxy error');
-//   });
-// });
-
-// httpServer.listen(HTTP_PORT, () => {
-//   console.log(`HTTP proxy server is running at port: ${HTTP_PORT}`);
-// });
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running at port : ${PORT}`);
-// });
-
-
-var fs = require('fs'); 
-var https = require('https');
-var options = { 
-    key: fs.readFileSync('../private-key.pem'), 
-    cert: fs.readFileSync('../certificate.pem'), 
-    ca: fs.readFileSync('../apig-cert.pem'), 
-    requestCert: true, 
-    rejectUnauthorized: true
+const corsOptions = {
+  origin: '*', // Adjust this for production to allow only specific origins
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
 };
-https.createServer(options, function (req, res) { 
-    res.writeHead(200); 
-    res.end("hello world\n"); 
-}).listen(443);
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Define your HTTPS server options
+const httpsOptions = {
+  key: fs.readFileSync('./private-key.pem'), // Update the path to your private key
+  cert: fs.readFileSync('./certificate.pem'), // Update the path to your certificate
+  ca: fs.readFileSync('./apig-cert.pem'), // Update the path to your CA certificate
+  requestCert: true,
+  rejectUnauthorized: true,
+};
+
+// Create an HTTPS server
+const httpsServer = https.createServer(httpsOptions, app);
+
+// Define a route for HTTPS server
+app.get('/getData', (req, res) => {
+  res.send("Hello from HTTPS server");
+});
+
+// Listen on port 443 for HTTPS traffic
+httpsServer.listen(443, () => {
+  console.log('HTTPS server is running on port 443');
+});
+
+// Define an HTTP server for redirection (optional)
+const http = require('http');
+
+const httpServer = http.createServer((req, res) => {
+  // Redirect all HTTP requests to HTTPS
+  res.writeHead(301, { 'Location': `https://${req.headers['host']}${req.url}` });
+  res.end();
+});
+
+// Listen on port 80 for HTTP traffic (for redirection to HTTPS)
+httpServer.listen(80, () => {
+  console.log('HTTP server for redirection is running on port 80');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`HTTP server is running on port ${PORT}`);
+});
